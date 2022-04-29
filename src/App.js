c8fa88a39bd80f8d28406f125796f73dd7c0d6b5
Oceano <oceanoleeo@gmail.com>
2022-04-29 11:45:23 +0300

    Git-React-start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+import Footer from './Footer';
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/Technologies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+  <div>
+          <ul>
+            <li>Jerkin off the Cunt
+            </li>
+            <li>Smoke dat swishes</li>
+            <li>Grab those tits</li>
+          </ul>
+        </div>);
+  }
+
+  export default Technologies;
