3a1baa30462cc3a2c01409b083d8ac0209501286
Oceano <oceanoleeo@gmail.com>
2022-05-23 20:33:46 +0300

    mapped data

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,32 +1,36 @@
 import React from "react";
 import s from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
 import Dialog from "./Dialog/Dialog";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Sosa'},
         {id: 2, name: 'Bosa'}
-        ]
+    ];
 
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: "zdrasu"},
-        {id: 2, message: "sto pischilos'"}
-    ]
+        {id: 2, message: "sto pischilos'"},
+        {id: 3, message: "GET UP SUKA!"}
+    ];
+
+    let dialogsElements = dialogs.map((d) => {
+        return <Dialog name={d.name} id={d.id}/>
+    });
+
+    let messagesElements = messages.map(m => <Message id={m.id} message={m.message}/>);
 
     return (
         <div className={s.dialogs}>
+
             <div className={s.dialogsItems}>
-                <Dialog name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <Dialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-
+                {messagesElements}
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,31 +4,33 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
 
-  let postData = [
-    {id: 1, message: "YOU'RE JUST A PIDORAS FROM A GULAG", likesCount: 88},
-    {id: 2, message: "sto pischilos' brat?", likesCount: 14},
-    {id: 3, message: "haraso...haraso...", likesCount: 1337},
-    {id: 4, message: "Srazu hocu postel", likesCount: 666},
-  ]
+    let posts = [
+        {id: 1, message: "YOU'RE JUST A PIDORAS FROM A GULAG", likesCount: 88},
+        {id: 2, message: "sto pischilos' brat?", likesCount: 14},
+        {id: 3, message: "haraso...haraso...", likesCount: 1337},
+        {id: 4, message: "Srazu hocu postel", likesCount: 666},
+    ]
+    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
-  return (
-      <div className={s.MyPosts_header}>
-        <h3>My Showdowns:</h3>
-        <div className={s.addPost}>
-          <textarea></textarea>
-          <button>Add post</button>
-        </div>
-    <div className={s.posts}>
-      <div className={s.item}>
-        <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+    return (
+        <div className={s.MyPosts_header}>
+            <h3>My Showdowns:</h3>
+            <div className={s.addPost}>
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+            <div className={s.posts}>
+                <div className={s.item}>
+                  {postsElements}
+                    {/* <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
         <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
         <Post message={postData[2].message} likesCount={postData[2].likesCount}/>
-        <Post message={postData[3].message} likesCount={postData[3].likesCount}/>
-      </div>
-      </div>
+        <Post message={postData[3].message} likesCount={postData[3].likesCount}/>*/}
+                </div>
+            </div>
 
-      </div>
-  )
+        </div>
+    )
 }
 
 export default MyPosts;
